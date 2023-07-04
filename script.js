a936eadb1ac9e992e14df0de256852e606076a9b
Nikhil Babhulkar <rider@pop-os.localdomain>
2023-07-04 05:36:37 +0530

    footer section get added and the site get dont

--- script.js
+++ script.js
@@ -34,3 +34,31 @@ var swiper = new Swiper(".home-slider", {
     },
     loop:true
   });
+
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+    breakpoints:{
+      0:{
+        slidesPerView:1,
+      },
+      640:{
+        slidesPerView:2,
+      },
+      768:{
+        slidesPerView:3,
+      },
+      1024:{
+        slidesPerView:4,
+      }
+    }
+  });
